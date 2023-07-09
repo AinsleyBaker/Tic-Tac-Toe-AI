ed9101bb0cf55d64b8f1b0069777d30a0dd1f9e0
Ainsley Baker <138183784+AinsleyBaker@users.noreply.github.com>
2023-07-09 11:15:12 +1000

    Update MediumAI.py

--- MediumAI.py
+++ MediumAI.py
@@ -2,6 +2,15 @@ import random
 import TicTacToe as ttt
 
 def mediumAI(board, symbol):
+    """
+    An AI with medium level difficulty
+
+    The AI will attempt to win the game if it has two symbols in a row.
+    It will block the opponent from winning if the opponent has two symbols in a row.
+    It will choose a corner position if available.
+    It will choose the center position if available.
+    Otherwise, it will make a random move.
+    """
     winning_move = find_winning_move(board, symbol)
     opp_move = opp_winning_move(board, symbol)
     legal_moves = ttt.get_legal_moves(board)
@@ -23,6 +32,9 @@ def mediumAI(board, symbol):
         return ttt.random_move(board, symbol)
     
 def opp_winning_move(board, symbol):
+    """
+    Finds the opponent's winning move on the tic-tac-toe board if possible.
+    """
     if symbol == "X":
         return find_winning_move(board, "0")
     else:
@@ -30,6 +42,9 @@ def opp_winning_move(board, symbol):
 
 
 def find_winning_move(board, symbol):
+    """
+    Finds a winning move for the given symbol on the tic-tac-toe board.
+    """
     board_config = ttt.get_board_config()
 
     for line in board_config:
